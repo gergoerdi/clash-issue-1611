9ae163ca52bbf84d33bd7438904240bda6ee1767
Dr. ERDI Gergo <gergo@erdi.hu>
2020-12-08 18:55:41 +0800

    Add `-fclash-debug DebugSilent`

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 stack build
-stack exec -- clash -isrc -outputdir _build/ --verilog src/Board.hs
+stack exec -- clash -isrc -outputdir _build/ --verilog src/Board.hs -fclash-debug DebugSilent 2>&1 | tee log.txt
