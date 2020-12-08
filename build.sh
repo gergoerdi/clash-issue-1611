81856d943840c1c651c580e9dbb93b7f93488b2a
Dr. ERDI Gergo <gergo@erdi.hu>
2020-12-08 18:57:48 +0800

    Increase inliner limit to 1000

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 stack build
-stack exec -- clash -isrc -outputdir _build/ --verilog src/Board.hs -fclash-debug DebugSilent 2>&1 | tee log.txt
+stack exec -- clash -isrc -outputdir _build/ --verilog src/Board.hs \
+      -fclash-debug DebugSilent \
+      -fclash-inline-limit=1000 \
+      2>&1 | tee log.txt
